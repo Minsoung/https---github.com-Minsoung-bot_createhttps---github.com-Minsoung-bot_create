3a433991b5f8578ee2c91e5d1c2a228e0c46ce8c
Minsoung <gggusckd7782@naver.com>
2022-03-06 22:43:43 +0900

    민성- 디자인 링크 수정

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ con.connect(function(err){
 // 3000 포트로 서버 오픈
 app.listen(3000, '0.0.0.0');
 
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/img'));
+app.use(express.static(__dirname + '/font'));
+
 app.get('/', function(req,res) {
 
     var requestIp = require('request-ip');
