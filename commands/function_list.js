9f8577532fe2c018e5352c2abf5c78e157ab8cdf
Minsoung <gggusckd7782@naver.com>
2022-03-09 02:59:02 +0900

    김민성_TT

--- commands/function_list.js
+++ commands/function_list.js
@@ -1,11 +1,31 @@
+// Null 확인 값이 null이면 true 아니면 false 값을 반환
+// 김민성
 gfn_isNull = function(value) {
     if (value == 'undefined' || value == "" || value == null || value == undefined || value == "null") {
-    return true;
-}
+        return true;
+    }
 
     return false;
 }
 
+// 날짜 값 가져오기 날 / 시간 / 분
+// 김민성
+gfn_getDate = function(value) {
+    var date = new Date();
+
+    if (value == "Day") {
+        return date.getDay();
+    } else if (value == "Hours") {
+        return date.getHours();
+    } else if (value == "Minutes") {
+        return date.getMinutes();
+    } else {
+        return false;
+    }
+}
+
+// 디스코드에 메세지 값 설정 후 현재 있는 채널에 메세지 출력
+// 김민성
 ufn_Send_Msg = function(msg, Discord, board) {
     const embed = new Discord.MessageEmbed()
     .setTitle(board.Title)
