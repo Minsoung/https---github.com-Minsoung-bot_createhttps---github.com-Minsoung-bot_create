5a5701d6a3b04cfb017c589b1a639cb6dfb3b02c
Minsoung1~ <gggusckd7782@naver.com>
2022-04-09 13:51:17 +0900

    작성자 : HDC 작업내용 : 스케줄러 완성

--- commands/schedule.js
+++ commands/schedule.js
@@ -5,26 +5,67 @@ const sheet_client = require('./google_db');
 const google_Master = require('./google_config.json');
 const con = require('./mysql_db');
 
-Schedule_Update =  function(msg, Discord) {
+Schedule_Update = function(msg, Discord) {
     
     // 화요일, 토요일 23시 스케줄러 업데이트
-    //run.dayOfWeek = [2,6];
-    //run.hour = 23;
-    run.second = 5;//test time
+    run.dayOfWeek = [2,6];
+    run.hour = 23;
+    //run.second = 30;//test time
 
     let timer = schedule.scheduleJob(run, function() {
+      var sql = "";
+
         const googleSheets = google.sheets({version : "v4"});
 
         let User_Nick_Name = new Array();
         let sheet_value = "";
         let request = "";
-        let response = "sheet15!D2:D177";
+        let response = "";//파티편성_ONE!D2:D175";
 
-        console.log(response);
-        for (var i = 0; i < 177; i++) {
-          User_Nick_Name.push({range: `sheet15!D${Number(i + 2)}`, values: [["미정자"]]});
+        for (var i = 0; i < 175; i++) {
+          User_Nick_Name.push({range: `파티편성_ONE!D${Number(i + 2)}`, values: [["미정자"]]});
         }
-        console.log(User_Nick_Name);
+
+        sheet_value = {
+              spreadsheetId : google_Master.spreadsheetId
+            , auth  : sheet_client
+            , valueInputOption : google_Master.valueInputOption
+        };
+
+        request = {
+          spreadsheetId : sheet_value.spreadsheetId
+          , auth : sheet_value.auth
+          , resource : { 
+            valueInputOption : sheet_value.valueInputOption
+          , data : User_Nick_Name
+          }
+        };
+
+        response = (googleSheets.spreadsheets.values.batchUpdate(request)).data;
+
+        console.log('미지정 업데이트');
+
+        sheet_value = {
+            spreadsheetId : google_Master.spreadsheetId
+          , range : "파티편성_ONE!H2:J175"
+          , auth  : sheet_client
+          , majorDimension : google_Master.majorDimension_Row
+        };
+        
+        request = {
+          spreadsheetId : sheet_value.spreadsheetId
+          , auth : sheet_value.auth
+          , resource : { ranges : [sheet_value.range]}
+        };
+
+        response = (googleSheets.spreadsheets.values.batchClear(request)).data; 
+        
+        console.log('ROW삭제');
+
+        for (var i = 0; i < 175; i++) {
+          User_Nick_Name.push({range: `파티편성_TOP!D${Number(i + 2)}`, values: [["미정자"]]});
+        }
+
         sheet_value = {
               spreadsheetId : google_Master.spreadsheetId
             , auth  : sheet_client
@@ -44,7 +85,7 @@ Schedule_Update =  function(msg, Discord) {
 
         sheet_value = {
             spreadsheetId : google_Master.spreadsheetId
-          , range : "sheet15!H2:J176"
+          , range : "파티편성_TOP!H2:J175"
           , auth  : sheet_client
           , majorDimension : google_Master.majorDimension_Row
         };
@@ -59,7 +100,7 @@ Schedule_Update =  function(msg, Discord) {
 
       sheet_value = {
           spreadsheetId : google_Master.spreadsheetId
-        , range : "sheet14!A2:AZ300"
+        , range : "참가자_ONE!A2:AZ300"
         , auth  : sheet_client
         , majorDimension : google_Master.majorDimension_Row
       };
@@ -71,8 +112,30 @@ Schedule_Update =  function(msg, Discord) {
       };
       
       response = (googleSheets.spreadsheets.values.batchClear(request)).data;
-      
-      console.log('클리어');
+
+      sheet_value = {
+        spreadsheetId : google_Master.spreadsheetId
+      , range : "참가자_TOP!A2:AZ300"
+      , auth  : sheet_client
+      , majorDimension : google_Master.majorDimension_Row
+    };
+    
+    request = {
+      spreadsheetId : sheet_value.spreadsheetId
+      , auth : sheet_value.auth
+      , resource : { ranges : [sheet_value.range]}
+    };
+    
+    response = (googleSheets.spreadsheets.values.batchClear(request)).data;
+    
+
+    sql  += " DELETE FROM USER_WARLIST";
+  
+    con.query(sql, function(err, result, fields) {
+      console.log('USER_WARLIST 클리어');
+    });
+    
+    console.log('클리어');
     });    
 }
 
